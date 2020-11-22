66388ee4b26b16c7e0f5c3636f82cea377d1e87c
benj9000 <49900294+benj9000@users.noreply.github.com>
2020-11-22 06:09:11 +0100

    Replaced MesloLGS font with Ubuntu Mono.

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ BASEDIR=$(dirname $0)
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${BASEDIR}/zsh/.zsh/themes/powerlevel10k
 
 # download nerd fonts
-wget -P ${BASEDIR}/fonts/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget -P ${BASEDIR}/fonts/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget -P ${BASEDIR}/fonts/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget -P ${BASEDIR}/fonts/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+wget -P ${BASEDIR}/fonts/.fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
+wget -P ${BASEDIR}/fonts/.fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Bold/complete/Ubuntu%20Mono%20Bold%20Nerd%20Font%20Complete%20Mono.ttf
+wget -P ${BASEDIR}/fonts/.fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular-Italic/complete/Ubuntu%20Mono%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
+wget -P ${BASEDIR}/fonts/.fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Bold-Italic/complete/Ubuntu%20Mono%20Bold%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
 # update fonts cache
 #fc-cache -f
 
