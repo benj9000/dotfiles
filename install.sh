311c42c27c1feed96492722fe5e8152d46bd0168
benj9000 <49900294+benj9000@users.noreply.github.com>
2020-11-28 02:57:34 +0100

    Added urxvt-resize-font, zsh-syntax-highlighting, zsh-history-substring-search and zsh-autosuggestions to the install script.

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 
 BASEDIR=$(dirname $0)
 
-# clone powerline theme repo
-git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${BASEDIR}/zsh/.zsh/themes/powerlevel10k
-
 # download nerd fonts
 wget -P ${BASEDIR}/fonts/.fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
 wget -P ${BASEDIR}/fonts/.fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Bold/complete/Ubuntu%20Mono%20Bold%20Nerd%20Font%20Complete%20Mono.ttf
@@ -12,4 +9,13 @@ wget -P ${BASEDIR}/fonts/.fonts https://github.com/ryanoasis/nerd-fonts/raw/mast
 wget -P ${BASEDIR}/fonts/.fonts https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Bold-Italic/complete/Ubuntu%20Mono%20Bold%20Italic%20Nerd%20Font%20Complete%20Mono.ttf
 # update fonts cache
 #fc-cache -f
+
+# download urxvt extensions
+wget -P ${BASEDIR}/urxvt/.urxvt/ext https://raw.githubusercontent.com/simmel/urxvt-resize-font/master/resize-font
+
+# clone some zsh package repos and the powerline theme repo
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${BASEDIR}/zsh/.zsh/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-history-substring-search.git ${BASEDIR}/zsh/.zsh/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${BASEDIR}/zsh/.zsh/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${BASEDIR}/zsh/.zsh/themes/powerlevel10k
 
