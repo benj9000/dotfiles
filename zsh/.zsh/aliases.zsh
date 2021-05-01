e969e977ca5cbe4da827751e786c309d47797474
benj9000 <49900294+benj9000@users.noreply.github.com>
2021-05-01 14:57:52 +0200

    Removed zsh aliases for verbose copying, moving and removing.

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -2,10 +2,6 @@ alias ls='ls --color=auto'
 alias l='ls -l'
 alias ll='ls -la'
 
-alias cp="cp -iv"
-alias mv="mv -iv"
-alias rm="rm -iv"
-
 alias gs='git status'
 alias gp='git pull'
 alias gc='git commit'
