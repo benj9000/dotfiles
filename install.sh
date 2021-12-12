d65d1ba767dc2c4304691a5e6b3d7a176d1a9205
benj9000 <49900294+benj9000@users.noreply.github.com>
2021-12-12 03:30:56 +0100

    Slightly adjusted installation of external fonts, packages etc.

--- install.sh
+++ install.sh
@@ -2,17 +2,18 @@
 
 BASEDIR=$(dirname $0)
 
-source external-packages.sh
+source external-files.sh
 
 echo "   ╔═══════════════════════════════════════════════════════════════════════╗"
 echo "   ║                         Installing fonts...                           ║"
 echo "   ╚═══════════════════════════════════════════════════════════════════════╝"
 
 FONT_NUMBER=${#FONTS[@]}
-for (( i=0; i<$FONT_NUMBER; i++)); do
-    # the suspected path and file name of the font in question
-    FONT_DEST=$BASEDIR/fonts/.fonts
 
+FONT_DEST=$BASEDIR/fonts/.fonts  # the path for the fonts
+mkdir -p $FONT_DEST
+
+for (( i=0; i<$FONT_NUMBER; i++)); do
     if [ ! -f "$FONT_DEST/${FONTS[$i]}" ]; then
         wget -P "$BASEDIR/fonts/.fonts" "${FONT_URLS[$i]}"
     else 
@@ -25,10 +26,11 @@ echo "   ║                    Installing urxvt extensions...                  
 echo "   ╚═══════════════════════════════════════════════════════════════════════╝"
 
 EXT_NUMBER=${#URXVT_EXTS[@]}
-for (( i=0; i<$EXT_NUMBER; i++)); do
-    # the suspected path and file name of the extension in question
-    EXT_DEST=$BASEDIR/urxvt/.urxvt/ext
 
+EXT_DEST=$BASEDIR/urxvt/.urxvt/ext  # the path for the extensions
+mkdir -p $EXT_DEST
+
+for (( i=0; i<$EXT_NUMBER; i++)); do
     if [ ! -f "$EXT_DEST/${URXVT_EXTS[$i]}" ]; then        
         wget -P "$EXT_DEST" "${URXVT_EXT_URLS[$i]}"
     else 
@@ -41,10 +43,11 @@ echo "   ║                      Installing zsh packages...                    
 echo "   ╚═══════════════════════════════════════════════════════════════════════╝"
 
 PACKAGE_NUMBER=${#ZSH_PACKAGES[@]}
-for (( i=0; i<$PACKAGE_NUMBER; i++)); do
-    # the suspected path and file name of the package in question
-    PACKAGE_DEST=$BASEDIR/zsh/.zsh
 
+PACKAGE_DEST=$BASEDIR/zsh/.zsh  # the path for the zsh packages
+mkdir -p $PACKAGE_DEST
+
+for (( i=0; i<$PACKAGE_NUMBER; i++)); do
     if [ ! -d "$PACKAGE_DEST/${ZSH_PACKAGES[$i]}" ]; then
         echo "DOWNLOAD $PACKAGE_DEST/${ZSH_PACKAGES[$i]}"     
         git clone --depth=1 "${ZSH_PACKAGE_URLS[$i]}" "$PACKAGE_DEST/${ZSH_PACKAGES[$i]}" 
@@ -61,7 +64,7 @@ stow --dir ${BASEDIR} --target=$HOME -R fonts
 stow --dir ${BASEDIR} --target=$HOME -R lf
 stow --dir ${BASEDIR} --target=$HOME -R rofi
 stow --dir ${BASEDIR} --target=$HOME -R urxvt
-stow --dir ${BASEDIR} --target=$HOME -R xresources
 # stow --dir ${BASEDIR} --target=$HOME -R vim
+stow --dir ${BASEDIR} --target=$HOME -R xresources
 stow --dir ${BASEDIR} --target=$HOME -R zsh
 
