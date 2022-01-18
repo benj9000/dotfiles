55f93e6ecf052ea2c653b9df232e0bb0c3a59365
benj9000 <49900294+benj9000@users.noreply.github.com>
2022-01-19 00:58:16 +0100

    Modified aliases for ls.

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,6 +1,8 @@
-alias ls='ls -h --color=auto --group-directories-first'
-alias l='ls -l'
-alias ll='ls -la'
+alias ls='ls --human-readable --color=auto --group-directories-first'
+alias l='ls -1'
+alias ll='ls -l --almost-all'
+alias lr='l --recursive'
+alias llr='ll --recursive'
 
 alias gs='git status'
 alias gp='git pull'
